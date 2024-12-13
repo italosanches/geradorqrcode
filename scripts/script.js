600f486c70c6966619d8f636323248100b9d240e
italosanches <italo_carvalho157@hotmail.com>
2024-12-13 10:39:32 -0300

    adicionado evento enter input

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,39 @@
 const buttonQrCode = document.querySelector("#button-qr");
-buttonQrCode.addEventListener("click",()=>{
+buttonQrCode.addEventListener("click", () => {
   generateQrCode();
-})
+});
+document.querySelector("#input-qr").addEventListener("keyup", (event) => {
+  event.preventDefault();
+  if (event.keyCode == 13) {
+    generateQrCode();
+  }
+});
 async function generateQrCode() {
   const containerQr = document.querySelector(".qr_code_img");
-  containerQr.textContent = ''
+  containerQr.textContent = "";
 
   const url = document.querySelector("#input-qr").value;
   let imgHtml;
   let imgBlob;
-  if(!url){
-    return
+  if (!url) {
+    return;
   }
-   try {
-    const response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`)
-    if(!response.ok){
+  try {
+    const response = await fetch(
+      `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`
+    );
+    if (!response.ok) {
       throw new Error(response.statusText);
-      
     }
     imgHtml = document.createElement("img");
-    imgBlob = await response.blob()
+    imgBlob = await response.blob();
     imgHtml.src = URL.createObjectURL(imgBlob);
-    imgHtml.onload =() =>{
+    imgHtml.onload = () => {
       containerQr.appendChild(imgHtml);
       containerQr.classList.add("active");
-      buttonQrCode.textContent ='Codigo gerado'
-      
-    }
-    
-  
-    
-   } catch (error) {
-    console.log(error)
-   }
+      buttonQrCode.textContent = "Codigo gerado";
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
