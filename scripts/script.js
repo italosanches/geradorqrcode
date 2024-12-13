5ea235dcf7e9042d3982cb3083c798a867fe7b4f
italosanches <italo_carvalho157@hotmail.com>
2024-12-12 22:20:31 -0300

    Projeto finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,36 @@
-const button = document.querySelector("#button-qr");
-button.addEventListener("click", async function () {
-  let containerQr = document.querySelector(".qr_code_img");
-  var url = document.querySelector("#input-qr").value;
-  if (url) {
-    while (containerQr.firstChild) {
-      containerQr.removeChild(containerQr.firstChild);
-    }
-    fetch(
-      `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`
-    )
-      .then((response) => response.blob())
-      .then((imgblob) => {
-        const imgHtml = document.createElement("img");
-        const imgUlr = URL.createObjectURL(imgblob);
-        imgHtml.src = imgUlr;
-        containerQr.appendChild(imgHtml);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } else {
+const buttonQrCode = document.querySelector("#button-qr");
+buttonQrCode.addEventListener("click",()=>{
+  generateQrCode();
+})
+async function generateQrCode() {
+  const containerQr = document.querySelector(".qr_code_img");
+  containerQr.textContent = ''
+
+  const url = document.querySelector("#input-qr").value;
+  let imgHtml;
+  let imgBlob;
+  if(!url){
+    return
   }
-  // try {
-  //   const response = await fetch(
-  //
-  //   );
-  //   if (!response.ok) {
-  //     throw new Error(response.status);
-  //   }
-  //   const result = await response.blob();
-  //   console.log(result);
-  // } catch (error) {
-  //   console.log(error.message);
-  // }
-});
+   try {
+    const response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`)
+    if(!response.ok){
+      throw new Error(response.statusText);
+      
+    }
+    imgHtml = document.createElement("img");
+    imgBlob = await response.blob()
+    imgHtml.src = URL.createObjectURL(imgBlob);
+    imgHtml.onload =() =>{
+      containerQr.appendChild(imgHtml);
+      containerQr.classList.add("active");
+      buttonQrCode.textContent ='Codigo gerado'
+      
+    }
+    
+  
+    
+   } catch (error) {
+    console.log(error)
+   }
+}
