7dfa4421855a50cde26fea123e672bafdd0763de
italosanches <italo_carvalho157@hotmail.com>
2024-12-16 17:36:28 -0300

    Ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,38 @@
 const buttonQrCode = document.querySelector("#button-qr");
 buttonQrCode.addEventListener("click", () => {
-  generateQrCode();
+	generateQrCode();
 });
 document.querySelector("#input-qr").addEventListener("keyup", (event) => {
-  event.preventDefault();
-  if (event.keyCode == 13) {
-    generateQrCode();
-  }
+	event.preventDefault();
+	if (event.keyCode == 13) {
+		generateQrCode();
+	}
 });
 async function generateQrCode() {
-  const containerQr = document.querySelector(".qr_code_img");
-  containerQr.textContent = "";
+	const containerQr = document.querySelector(".qr_code_img");
+	containerQr.textContent = "";
 
-  const url = document.querySelector("#input-qr").value;
-  let imgHtml;
-  let imgBlob;
-  if (!url) {
-    return;
-  }
-  try {
-    const response = await fetch(
-      `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`
-    );
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    imgHtml = document.createElement("img");
-    imgBlob = await response.blob();
-    imgHtml.src = URL.createObjectURL(imgBlob);
-    imgHtml.onload = () => {
-      containerQr.appendChild(imgHtml);
-      containerQr.classList.add("active");
-      buttonQrCode.textContent = "Codigo gerado";
-    };
-  } catch (error) {
-    console.log(error);
-  }
+	const url = document.querySelector("#input-qr").value;
+
+	if (!url) {
+		return;
+	}
+	try {
+		const response = await fetch(
+			`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`
+		);
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+		const imgHtml = document.createElement("img");
+		const imgBlob = await response.blob();
+		imgHtml.src = URL.createObjectURL(imgBlob);
+		imgHtml.onload = () => {
+			containerQr.classList.remove("hidden");
+			containerQr.appendChild(imgHtml);
+			buttonQrCode.textContent = "Codigo gerado";
+		};
+	} catch (error) {
+		console.log(error);
+	}
 }
